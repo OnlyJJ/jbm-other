0266db778d5c7e6fe093872a2088fb57206190a4
shao <370083084@qq.com>
2018-10-12 17:06:23 +0800

    t

---
+++ src/main/java/com/lm/jbm/thread/SendGiftByGoldThread.java
@@ -0,0 +1,25 @@
+package com.lm.jbm.thread;
+
+
+
+import com.lm.jbm.service.JmService;
+import com.lm.jbm.utils.RandomUtil;
+
+
+
+
+public class SendGiftByGoldThread implements Runnable {
+
+	public SendGiftByGoldThread() {
+	}
+
+	public void run() {
+		try {
+			String[] userIds = RandomUtil.getUserIds();
+			JmService.sendGiftByGold(userIds);
+		} catch (Exception e) {
+			System.err.println("批量送礼异常，退出!");
+			Thread.interrupted();
+		}
+	}
+}
